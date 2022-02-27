e9c0bfc60b9600011fba6668a0a8379ad34d0fd2
m_ronald <michael.ronald@allycode.com.br>
2022-02-27 10:19:21 -0300

    Fazendo avaliador Ãpegar os 3 maiores lances do leilão

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,11 +2,14 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private float $maiorValor = -INF;
+    private float $menorValor = +INF;
+    private array $maioresValores;
 
     public function avalia(Leilao $leilao): void
     {
@@ -14,11 +17,33 @@ class Avaliador
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
             }
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
         }
+
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresValores = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresValores(): array
+    {
+        return $this->maioresValores;
     }
 }
